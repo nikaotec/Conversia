40afbef4469ed5e8e029059704b399918b3a4b26
Venancio <antoniovenancio10@gmail.com>
2025-06-05 11:18:27 -0300

    ajsutes

--- src/main/java/com/avs/conversia/controller/teste.java
+++ src/main/java/com/avs/conversia/controller/teste.java
@@ -1,23 +1,21 @@
 package com.avs.conversia.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class teste {
 
    
-    @GetMapping
-    public String getMethodName() {
-        return "Ola deu tudo certo ";
-    }
+    // @GetMapping
+    // public String getMethodName() {
+    //     return "Ola deu tudo certo ";
+    // }
 
-    @GetMapping("/teste")
+    @GetMapping("/")
     public String getTeste() {
-        return "Ola deu tudo certo no teste";
+        return "index.html";
     }
     
 }
